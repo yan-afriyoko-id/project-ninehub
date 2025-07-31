415d7b19bcafcbc1d16fd62c877d7dcbc054bf54
Fawwaz Al Farisi <fawwazalfarisi.works@gmail.com>
2025-07-31 12:32:06 +0700

    feat(permission): implement CRUD Permission with SOLID principles and comprehensive testing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,10 +6,14 @@ use App\Repositories\TenantRepository;
 use App\Repositories\Interfaces\TenantRepositoryInterface;
 use App\Repositories\ModuleRepository;
 use App\Repositories\Interfaces\ModuleRepositoryInterface;
+use App\Repositories\PermissionRepository;
+use App\Repositories\Interfaces\PermissionRepositoryInterface;
 use App\Services\TenantService;
 use App\Services\Interfaces\TenantServiceInterface;
 use App\Services\ModuleService;
 use App\Services\Interfaces\ModuleServiceInterface;
+use App\Services\PermissionService;
+use App\Services\Interfaces\PermissionServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +25,10 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(TenantRepositoryInterface::class, TenantRepository::class);
         $this->app->bind(ModuleRepositoryInterface::class, ModuleRepository::class);
+        $this->app->bind(PermissionRepositoryInterface::class, PermissionRepository::class);
         $this->app->bind(TenantServiceInterface::class, TenantService::class);
         $this->app->bind(ModuleServiceInterface::class, ModuleService::class);
+        $this->app->bind(PermissionServiceInterface::class, PermissionService::class);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TenantController;
 use App\Http\Controllers\ModuleController;
+use App\Http\Controllers\PermissionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -28,4 +29,18 @@ Route::prefix('modules')->middleware('auth:sanctum')->group(function () {
     Route::get('/{module}', [ModuleController::class, 'show'])->middleware('module.permission:plan-management.view');
     Route::put('/{module}', [ModuleController::class, 'update'])->middleware('module.permission:plan-management.edit');
     Route::delete('/{module}', [ModuleController::class, 'destroy'])->middleware('module.permission:plan-management.delete');
+});
+
+// Permission Management Routes
+Route::prefix('permissions')->middleware('auth:sanctum')->group(function () {
+    Route::get('/', [PermissionController::class, 'index'])->middleware('module.permission:permission-management.view');
+    Route::post('/', [PermissionController::class, 'store'])->middleware('module.permission:permission-management.create');
+    Route::get('/statistics', [PermissionController::class, 'statistics'])->middleware('module.permission:permission-management.view');
+    Route::get('/{id}', [PermissionController::class, 'show'])->middleware('module.permission:permission-management.view');
+    Route::put('/{id}', [PermissionController::class, 'update'])->middleware('module.permission:permission-management.edit');
+    Route::delete('/{id}', [PermissionController::class, 'destroy'])->middleware('module.permission:permission-management.delete');
+    Route::get('/guard/{guard}', [PermissionController::class, 'byGuard'])->middleware('module.permission:permission-management.view');
+    Route::get('/module/{moduleSlug}', [PermissionController::class, 'byModule'])->middleware('module.permission:permission-management.view');
+    Route::get('/search', [PermissionController::class, 'search'])->middleware('module.permission:permission-management.view');
+    Route::post('/sync', [PermissionController::class, 'sync'])->middleware('module.permission:permission-management.create');
 });
