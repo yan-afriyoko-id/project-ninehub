0af7ad864067c21b97281b023f541483f056031a
Fawwaz Al Farisi <fawwazalfarisi.works@gmail.com>
2025-08-05 10:58:42 +0700

    fix: add company_id to migration

--- database/migrations/2025_07_30_065358_create_contacts_table.php
+++ database/migrations/2025_07_30_065358_create_contacts_table.php
@@ -17,6 +17,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->string('job_title')->nullable();
+            $table->foreignId('company_id')->nullable()->constrained()->onDelete('set null');
             $table->timestamps();
         });
     }
